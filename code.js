035a1ac7a3380e3e29df1b74c466802626c41244
Jules Désir <jules.desir@etu.minesparis.psl.eu>
2023-03-01 15:40:37 +0100

    Début, ne fonctionne pas car counters non défini

--- code.js
+++ code.js
@@ -1,10 +1,3 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // Initial clean up. DO NOT REMOVE.
-  initialCleanup();
-
-  // Hey! Pssst! In here ...
-});
-
 /**
  * Cleans up the document so that the exercise is easier.
  *
@@ -21,4 +14,70 @@ function initialCleanup() {
   for (const node of nodesToRemove) {
     node.remove();
   }
+}
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  // Initial clean up. DO NOT REMOVE.
+  initialCleanup();
+
+  // Hey! Pssst! In here ...
+  document.getElementById("btn-add-line").addEventListener("click",
+    () => {
+      console.log("click ici !")
+    }
+  )
+  for (let i=0; i<10; i++) {
+    square = document.createElement("div")
+    addBehavior(square, counters)
+    document.getElementById("grid").appendChild(square)
+    document.appendChild()
+    counters.total++
+    counters.original++
+    synchCounters(counters)
+    }
+
+  document.querySelectionAll("#grid div").forEach(square =>
+    square.addEventListener("click", (event) => {
+      event.target.style.backgroundColor="green"
+    })
+  )
+
+  for (let square of document.getElementById("grid").children) {
+    square.addEventListener("click", (event) => {
+      event.target.style.backgroundColor="green"
+    })
+  }
+});
+
+
+function synchCounters(counters) {
+  for (key in counters) {
+    document.getElementById(key).textContent = counters[key]
+  }
+}
+
+
+function addBehavior(square, counters) {
+  square.addEventListener("click", (event) => {
+    event.target.style.backgroundColor="green"
+    counters.clicked++
+    if (event.target.classList.contains("painted")) {
+      counters.hovered--
+    }
+    else {
+      counters.original--
+    }
+    synchCounters(counters)
+  })
+
+  square.addEventListener("mouseenter", (event) => {
+    if (!event.target.classList.contains("painted"))
+    {
+      event.target.classList.add("painted");
+      counters.hovered++
+      counters.original--
+      synchCounters(counters)
+    }
+  })
 }
